b2f0269fd5fd5a0782b3d39c544d120cd2398d09
Georgi <shr41bm4n@hotmail.com>
2024-09-12 20:20:49 +0300

    ChatSessionPage

--- AI_Chat_API/Views/ChatSessionPage.xaml.cs
+++ AI_Chat_API/Views/ChatSessionPage.xaml.cs
@@ -5,12 +5,10 @@ namespace AI_Chat_API.Views
 {
     public partial class ChatSessionPage : ContentPage
     {
-        private readonly ChatSessionViewModel _viewModel;
-
         public ChatSessionPage(ChatSessionViewModel viewModel)
         {
             InitializeComponent();
-            BindingContext = _viewModel = viewModel;
+            BindingContext = viewModel;
         }
     }
 }
